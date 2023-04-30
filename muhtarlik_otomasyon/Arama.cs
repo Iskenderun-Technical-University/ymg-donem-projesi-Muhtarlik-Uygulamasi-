0783aad57ee307e45e8f489cbfe65bdd31a686e7
erayasar <93430897+erayasar@users.noreply.github.com>
2023-04-30 12:38:17 +0300

    Update Arama.cs

--- muhtarlik_otomasyon/Arama.cs
+++ muhtarlik_otomasyon/Arama.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
 
-namespace muhtarlik_otomasyon
+namespace muhtarlik_otomasyonss
 {
     public partial class Arama : Form
     {
